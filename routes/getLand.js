3662a0eaeee493f94fb69a3f317769e8400661b2
Tim Rus <tim.rus@ad.mesi.si>
2022-09-03 17:22:04 +0200

    fixes

--- routes/getLand.js
+++ routes/getLand.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
-const Land = require('../model/land');
+const Land = require('../model/Land');
 const { result } = require('lodash');
 
 // Cant acces if user is not verifyed

6878ef3ec0115cd25636e27adbcea63658f4aef3
Tim Rus <tim.rus@ad.mesi.si>
2022-09-03 21:13:03 +0200

    Updates on API

--- routes/auth.js
+++ routes/auth.js
@@ -42,19 +42,28 @@ router.post('/login', async (req, res) => {
     const { error } = loginValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    let isError = false;
+
     // Checking if the email doesnt exist
     const user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(400).send('INVALID EMAIL');
+    if (!user) {
+        isError = true;
+        return res.status(400).send('Invalid email');
+    }
 
     // Password is correct
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('INVALID PASSWORD');
+
+    if (!validPassword) {
+        isError = true;
+        return res.status(400).send('Invalid password');
+    }
 
     // Create and asign a token
     const token = jwt.sign({_id: user.id}, process.env.TOKEN_SECRET);
     res.setHeader('auth-token', token);
 
-    res.send({user_token: token, user});
+    res.send({error: isError, user_token: token, user});
     res.status(200);
 });
 

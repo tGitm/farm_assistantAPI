cfd686e86584df5d8217b64b6c61d0dfd1c56beb
Tim <timrus99@gmail.com>
2022-09-08 19:41:01 +0200

    API updates

--- routes/Lands/getLand.js
+++ routes/Lands/getLand.js
@@ -1,21 +1,9 @@
 const router = require('express').Router();
 const verify = require('../verifyToken');
-
-//const mongoose = require('../../db.js')
 const Land = require('../../model/Land.js')
-
-// const mongoURL = process.env.DB_CONNECT;
-
-// MongoClient.connect(mongoURL, function (err, client) {
-//     if (err) throw err;
-//     database = client.db('farmAssistant');
-// });
 
 // Get all lands
 router.get('/get-lands', async (req, res) => {
-    // const result = await database.collection('geojson').find({});
-    // if (!result) return res.status(500).send({ok: false})
-    // return res.send({ok: true, data: result})
 
     const result = await Land.find({});
     if (!result) return res.status(500).send({ok: false})
@@ -25,7 +13,6 @@ router.get('/get-lands', async (req, res) => {
 // Get geometry data for specific KMID
 router.get('/get-geometry/:id', async (req, res) => {
     // console.log("req.params.id", typeof parseInt(req.params.id));
-
     const result = await Land.find({'properties.KMG_MID': parseInt(req.params.id)}, {geometry: 1})
     
     if (!result) return res.status(500).send({ok: false})

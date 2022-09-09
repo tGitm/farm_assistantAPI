8a9b432147e6e4888f2872e363450cbe97079c41
Tim <timrus99@gmail.com>
2022-09-09 12:04:11 +0200

    upd

--- routes/userAccount.js
+++ routes/userAccount.js
@@ -32,6 +32,7 @@ router.put("/edit/:id", async (req, res) => {
             const user = await User.findByIdAndUpdate(conditions, updatedUser, { new: true });
             console.log('user: ' + user);
 
+
             res.send(user);
             res.status(200).json(user);
         } catch (err) {

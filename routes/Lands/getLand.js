bbeab496829a7dee6f3b69ebe1e917e505b9f67d
Tim <timrus99@gmail.com>
2022-09-10 10:14:18 +0200

    updates on lands

--- routes/Lands/getLand.js
+++ routes/Lands/getLand.js
@@ -7,17 +7,25 @@ router.get('/get-lands', async (req, res) => {
 
     const result = await Land.find({});
     if (!result) return res.status(500).send({ok: false})
-    return res.send({ok: true, data: result})
+    return res.send({ok: true, result})
 });
 
 // Get geometry data for specific KMID
 router.get('/get-geometry/:id', async (req, res) => {
     // console.log("req.params.id", typeof parseInt(req.params.id));
-    const result = await Land.find({'properties.KMG_MID': parseInt(req.params.id)}, {geometry: 1})
+    const result = await Land.find({'properties.KMG_MID': parseInt(req.params.id)})  //, {geometry: 1})
     
+    if (!result) return res.status(500).send({ok: false})
+    return res.send({ok: true, result})
+});
+
+// Get geometry data for specific land id
+router.get('/get-geometry/:id', async (req, res) => {
+    // console.log("req.params.id", typeof parseInt(req.params.id));
+    const result = await Land.find({'properties.KMG_MID': parseInt(req.params.id)}, {geometry: 1})
+
     if (!result) return res.status(500).send({ok: false})
     return res.send({ok: true, data: result})
 });
-
 
 module.exports = router;

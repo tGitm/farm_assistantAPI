8f26168b9a5a6a65c11f6e4675711f4f358bd28c
Tim <timrus99@gmail.com>
2022-10-16 14:32:11 +0200

    fixed responsing

--- routes/Chores/addPlotChores.js
+++ routes/Chores/addPlotChores.js
@@ -11,10 +11,10 @@ router.post('/add-chore', verify, async (req, res, next) => {
     const newChore = new Chores(req.body);
     try {
         const savedWork = await newChore.save();
-        res.send({worklist_id: newChore._id, chore: newChore})
-        res.status(200).json(savedWork);
+        return res.status(200).json({worklist_id: savedWork._id, chore: savedWork})
+        //res.status(200).json(savedWork);
     } catch(e) {
-        res.status(500).json(e);
+        return res.status(500).json(e);
     }
 });
 
@@ -24,12 +24,12 @@ router.put("/update-land-work/:id", verify, async (req, res) => {
         const work = await Worklist.findById(req.params.id);
         if (work.user_id === req.body.user_id) {
             await work.updateOne({ $set: req.body });
-            res.status(200).json("work has been updated");
+            return res.status(200).json("work has been updated");
         } else {
-            res.status(403).json("you can update only your work");
+            return res.status(403).json("you can update only your work");
         }
     } catch (e) {
-        res.status(500).json(e);
+        return res.status(500).json(e);
     }
 });
 
@@ -39,7 +39,7 @@ router.put("/update-land-work/:id", verify, async (req, res) => {
 router.delete("/delete/:id", async (req, res) => {
     try {
         await Chores.remove({_id: req.params.id}).exec()
-        res.status(200).json({message: "Chore has been deleted"});
+        return res.status(200).json({message: "Chore has been deleted"});
     } catch (err) {
         return res.status(500).json(err);
     }
--- routes/userAccount.js
+++ routes/userAccount.js
@@ -28,7 +28,7 @@ router.put("/edit/:id", async (req, res) => {
         const user = await User.findByIdAndUpdate(req.params.id, {
             $set: req.body,
         });
-        res.status(200).json({user: user});
+        return res.status(200).json({user: user});
     } catch (err) {
         return res.status(500).json(err);
     }

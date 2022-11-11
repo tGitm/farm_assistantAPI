ee466cb55f9e11635a6ac0ee1b765b6c4e71631f
Tim <timrus99@gmail.com>
2022-11-11 20:29:37 +0100

    Editing works

--- routes/Chores/addPlotChores.js
+++ routes/Chores/addPlotChores.js
@@ -7,7 +7,7 @@ const {ObjectId} = require("mongodb");
 // Cant post if user is not verified
 
 // add new work on land
-router.post('/add-chore', verify, async (req, res, next) => {
+router.post('/add-chore', async (req, res, next) => {
     const newChore = new Chores(req.body);
     try {
         const savedWork = await newChore.save();
@@ -19,20 +19,15 @@ router.post('/add-chore', verify, async (req, res, next) => {
 });
 
 // update work on specific land with user_id
-router.put("/update-land-work/:id", verify, async (req, res) => {
+router.put("/update-land-work/:id", async (req, res) => {
     try {
-        const work = await Worklist.findById(req.params.id);
-        if (work.user_id === req.body.user_id) {
-            await work.updateOne({ $set: req.body });
-            return res.status(200).json("work has been updated");
-        } else {
-            return res.status(403).json("you can update only your work");
-        }
+        const work = await Chores.findById(req.params.id);
+        await work.updateOne({ $set: req.body });
+        return res.status(200).json({message: "chore updated", chore: work});
     } catch (e) {
         return res.status(500).json(e);
     }
 });
-
 
 
 // delete work for specific land with user_id
